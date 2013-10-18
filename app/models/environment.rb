add66d6b47ab7b8aaffc6d940d947f83bb1b8b66
Eduardo Tourinho Edington <eduardo.edington@serpro.gov.br>
2013-10-18 16:42:05 -0300

    Removed EnvironmentStatisticsBlock references from Core.

--- app/models/environment.rb
+++ app/models/environment.rb
@@ -157,7 +157,6 @@ class Environment < ActiveRecord::Base
 
     # "left" area
     env.boxes[1].blocks << LoginBlock.new
-    env.boxes[1].blocks << EnvironmentStatisticsBlock.new
     env.boxes[1].blocks << RecentDocumentsBlock.new
 
     # "right" area
