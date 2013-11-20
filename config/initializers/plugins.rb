601fb457e5520028b1c726ac63275d65932b29f6
Leandro Nunes dos Santos <leandro.santos@serpro.gov.br>
2013-11-20 16:24:36 -0300

    merging with next colivre

--- config/initializers/plugins.rb
+++ config/initializers/plugins.rb
@@ -4,6 +4,7 @@ require 'noosfero/plugin/manager'
 require 'noosfero/plugin/active_record'
 require 'noosfero/plugin/mailer_base'
 require 'noosfero/plugin/settings'
+require 'noosfero/plugin/spammable'
 Noosfero::Plugin.init_system if $NOOSFERO_LOAD_PLUGINS
 
 if Rails.env == 'development' && $NOOSFERO_LOAD_PLUGINS
