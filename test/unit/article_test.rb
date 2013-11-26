4a9c9fb1d56617147a1cfcbcb2871a36d4ea2a90
Leandro Nunes dos Santos <leandro.santos@serpro.gov.br>
2013-11-26 17:48:00 -0300

    fixing test of article

--- test/unit/article_test.rb
+++ test/unit/article_test.rb
@@ -1744,15 +1744,6 @@ class ArticleTest < ActiveSupport::TestCase
     assert_nil article.author_id
   end
 
-  should 'save image on create article' do
-    assert_difference Article, :count do
-      p = Article.create!(:name => 'test', :image_builder => {
-        :uploaded_data => fixture_file_upload('/files/rails.png', 'image/png')
-      }, :profile_id => @profile.id)
-      assert_equal p.image(true).filename, 'rails.png'
-    end
-  end
-
   should 'return articles with specific types' do
     Article.delete_all
 
