f510c8440bbeef88ef14f377a3bc84797feceffb
Victor Costa <vfcosta@gmail.com>
2013-11-29 16:38:21 -0300

    Touch container block on save child block

--- plugins/container_block/lib/ext/block.rb
+++ plugins/container_block/lib/ext/block.rb
@@ -9,4 +9,13 @@ class Block
 
   alias_method_chain :owner, :container_block_plugin
 
+  after_save :touch_parent_container_block
+
+  def touch_parent_container_block
+    owner = owner_without_container_block_plugin
+    if owner.kind_of?(ContainerBlock)
+      owner.touch
+    end
+  end
+
 end
