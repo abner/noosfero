0ca453b578125770ef921fe6045cd117149e51a0
Victor Costa <vfcosta@gmail.com>
2013-12-06 10:29:16 -0300

    Remove themes dirs from view paths
    Themes must use theme_include method to render rhtml files (instead of using render directly).
    This avoids conflict of files with same name at different themes and reduces the amount of paths in view_paths.

--- config/initializers/view_paths.rb
+++ config/initializers/view_paths.rb
@@ -1,9 +1,3 @@
 Noosfero::Plugin.all.each do |plugin|
   ActionController::Base.view_paths.unshift(plugin.constantize.view_path)
 end
-
-themes = Dir.glob(File.join(Rails.root, 'public', 'designs', 'themes', '*'))
-themes += Dir.glob(File.join(Rails.root, 'public', 'user_themes', '*'))
-themes.select { |entry| File.directory?(entry) }.each do |dir|
-  ActionController::Base.view_paths.unshift(dir)
-end
