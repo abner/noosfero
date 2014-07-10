5c973eeba7df9adf0daf1d9e25978dc207cb9f47
Francisco Marcelo de Araújo Lima Júnior <francisco.lima-junior@serpro.gov.br>
2014-07-10 18:36:43 -0300

    undo removed attributes

--- plugins/anti_spam/lib/anti_spam_plugin/comment_wrapper.rb
+++ plugins/anti_spam/lib/anti_spam_plugin/comment_wrapper.rb
@@ -1,4 +1,8 @@
 class AntiSpamPlugin::CommentWrapper < AntiSpamPlugin::Wrapper
+  alias_attribute :author, :author_name
+  alias_attribute :user_ip, :ip_address
+  alias_attribute :content, :body
+
   rakismet_attrs :author => :author_name,
                  :user_ip => :ip_address,
                  :content => :body
--- plugins/anti_spam/lib/anti_spam_plugin/suggest_article_wrapper.rb
+++ plugins/anti_spam/lib/anti_spam_plugin/suggest_article_wrapper.rb
@@ -1,4 +1,9 @@
 class AntiSpamPlugin::SuggestArticleWrapper < AntiSpamPlugin::Wrapper
+  alias_attribute :author, :name
+  alias_attribute :author_email, :email
+  alias_attribute :user_ip, :ip_address
+  alias_attribute :content, :article_body
+
   rakismet_attrs :author => ':name',
                  :author_email => :email,
                  :user_ip => :ip_adress,
