78559c59aaa9d90745a485704287adfa2b10df00
Francisco Marcelo de Araújo Lima Júnior <francisco.lima-junior@serpro.gov.br>
2014-07-10 17:50:46 -0300

    fix accessors

--- plugins/anti_spam/lib/anti_spam_plugin/comment_wrapper.rb
+++ plugins/anti_spam/lib/anti_spam_plugin/comment_wrapper.rb
@@ -1,7 +1,7 @@
 class AntiSpamPlugin::CommentWrapper < AntiSpamPlugin::Wrapper
-  alias_attribute :author, :author_name
-  alias_attribute :user_ip, :ip_address
-  alias_attribute :content, :body
+  rakismet_attrs :author => :author_name,
+                 :user_ip => :ip_address,
+                 :content => :body
 
   def self.wraps?(object)
     object.kind_of?(Comment)
--- plugins/anti_spam/lib/anti_spam_plugin/suggest_article_wrapper.rb
+++ plugins/anti_spam/lib/anti_spam_plugin/suggest_article_wrapper.rb
@@ -1,8 +1,8 @@
 class AntiSpamPlugin::SuggestArticleWrapper < AntiSpamPlugin::Wrapper
-  alias_attribute :author, :name
-  alias_attribute :author_email, :email
-  alias_attribute :user_ip, :ip_address
-  alias_attribute :content, :article_body
+  rakismet_attrs :author => ':name',
+                 :author_email => :email,
+                 :user_ip => :ip_adress,
+                 :content => :article_body
 
   def self.wraps?(object)
     object.kind_of?(SuggestArticle)
