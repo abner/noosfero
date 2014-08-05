fd6bf2c6d3006946f4297d9c123bceb01931b0a4
Victor Costa <vfcosta@gmail.com>
2014-08-05 14:34:56 -0300

    Added includes in models to prevent n+1 queries
    N+1 queries found with the help of bullet gem

--- app/models/person.rb
+++ app/models/person.rb
@@ -468,7 +468,7 @@ roles] }
   end
 
   def already_reported?(profile)
-    abuse_reports.any? { |report| report.abuse_complaint.reported == profile && report.abuse_complaint.opened? }
+    abuse_reports.includes({:abuse_complaint => :reported}).any? { |report| report.abuse_complaint.reported == profile && report.abuse_complaint.opened? }
   end
 
   def register_report(abuse_report, profile)
